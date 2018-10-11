e5aaeb5f54c4d4b37837d614fbb6f933bb472083
Emily McBee <636694>
2018-10-11 08:10:40 -0400

    third

--- js.js
+++ js.js
@@ -2,6 +2,14 @@ $(document).ready(main);
 
 function main()	{
     
-    
+    var log = [
+        
+        {
+            journal: "lorem ipsum",
+            date: "October 11th, 2018",
+            time: "8:00",
+        }
+        
+    ];
     
 }
